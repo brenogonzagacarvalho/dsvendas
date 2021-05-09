8f5dc3a2ab7c555187b047cc69e9e172745bec01
Breno Gonzaga <brenogonzaga96@gmail.com>
2021-05-08 23:28:17 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div >
+            <div >
+
+                <h1>Pagina Home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
